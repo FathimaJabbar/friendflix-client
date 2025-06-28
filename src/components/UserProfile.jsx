bcad3b0642c0161fa631c684f55260693434a251
Fathima jabbar <fathimajabbar27@gmail.com>
2025-06-28 08:54:59 +0400

    Describe your changes

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React from 'react';
 
-export default function UserProfile({ user }) {
-  if (!user) return <p>No user logged in.</p>;
+function UserProfile({ user }) {
+  if (!user) return <p>Loading profile...</p>;
 
   return (
-    <div style={{ maxWidth: 600 }}>
-      <h2>Hello, {user.displayName || "Friend"}!</h2>
-      <p>Email: {user.email}</p>
-      <p>User ID: {user.uid}</p>
-      {/* Add more profile details here if you want */}
+    <div>
+      <h2>{user.displayName || "Friend"}</h2>
+      <h3>Your Selected Movies:</h3>
+      <ul>
+        {user.selectedMovies?.map(movie => (
+          <li key={movie.id}>{movie.title}</li>
+        )) || <li>No movies selected</li>}
+      </ul>
     </div>
   );
 }
+
+export default UserProfile;
